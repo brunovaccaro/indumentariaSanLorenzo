58c9c7540e9503727a4ce9b7b4b4b4d726f59445
vaccarobruno <bruno.vaccaro.1992@gmail.com>
2023-05-18 11:38:33 -0300

    agrego comentarios al JS

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-
+// CREO LA CLASE CONSTRUCTORA DE OBJETOS
 class Prenda {
   constructor(id, descripcion, precio, imagen) {
     this.id = id;
@@ -9,7 +9,7 @@ class Prenda {
   }
 }
 
-
+// CREO EL LISTADO DE PRODUCTOS
 const buzoArqueroNaranja = new Prenda(1, "Buzo arquero naranja", 25000, "img/buzoArqueroNaranja.png");
 const buzoArqueroVerde = new Prenda(2, "Buzo arquero verde", 25000, "img/buzoArqueroVerde.png");
 const buzoEntrenamiento = new Prenda(3, "Buzo de entrenamiento", 20000, "img/buzoEntrenamiento.jpg");
@@ -27,25 +27,33 @@ const remeraEntrenamiento = new Prenda(14, "Remera entrenamiento", 11000, "img/r
 const remeraPreMatch = new Prenda(15, "Remera prematch", 13000, "img/remeraPreMatch.png");
 const rompevientoAzul = new Prenda(16, "Rompeviento azul", 28000, "img/rompevientoAzul.png");
 
+// INSERO ESOS PRODUCTOS EN UN ARRAY
 const productos = [buzoArqueroNaranja, buzoArqueroVerde, buzoEntrenamiento, buzoLiso, camisetaSuplente, camisetaTitular, camperaEntrenamiento, camperaSalida, camperonAzul, camperonNegro, musculosaAzul, musculosaRoja, pantalonLargo, remeraEntrenamiento, remeraPreMatch, rompevientoAzul];
 
+// CREO EL ARRAY DE PRODUCTOS VACIO PARA QUE EL USUARIO VAYA AGREGANDO ALLÍ LOS QUE DESEE
 let carrito = [];
 
+// CREACION DE ALGUNAS CONSTANTES QUE UTILIZARE EN LAS FUNCIONES
 const cajaProductos = document.querySelector("#cajaProductos");
 const mostrarCarrito = document.querySelector("#mostrarCarrito");
 const cajaCarrito = document.querySelector("#cajaCarrito");
 const montoTotalDelCarrito = document.querySelector("#montoTotalDelCarrito");
 
-
+// SI HAY PRODUCTOS ALMACENADOS EN EL LOCAL STORAGE, LOS MUESTRO EN EL CARRITO, PARA QUE NO SE ELIMINEN CUANDO EL USUARIO CIERRA EL SITIO
 if (localStorage.getItem("carrito")) {
   carrito = JSON.parse(localStorage.getItem("carrito"));
 }
 
-
+// CREO LA FUNCION QUE MOSTRARÁ LOS PRODUCTOS EN LA PAGINA
 const mostrarProductos = () => {
+
+  // ITERO SOBRE LOS PRODUCTOS, Y CREO LAS FILAS Y COLUMNAS UTILIZANDO EL SISTEMA BOOTSTRAP A TRAVES DEL AGREGADO DE CLASES. PREVIAMENTE CREO UN ELEMENTO CONTENEDOR EN EL HTML PARA CADA UNA DE LAS TARJETAS
   productos.forEach(producto => {
     const tarjeta = document.createElement("div");
     tarjeta.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+
+    // LUEGO, A TRAVES DEL DOM, CREO POR CADA UNO DE ESOS PRODUCTOS UNA TARJETA QUE SE UBICA EN LAS COLUMNAS CREADAS, DE MANERA QUE SI AGREGO O ELIMINO PRODUCTOS, AUTOMATICAMENTE SE ACTUALICE SIN NECESIDAD DE MODIFICAR EL HTML Y CSS, NI TAMPOCO EL JS (EXCEPTO POR EL AGREGADO POTENCIAL DE ALGUN PRODUCTO AL ARRAY DE PRODUCTOS)
+    // PARA ELLO UTILIZO PLANTILLAS LITERALES HACIENDO MENCION DE CADA OBJETO Y SU PROPIEDAD SEGUN NECESIDAD, OPITIMIZANDO A FUTURO LA EXTENSION DEL CODIGO
     tarjeta.innerHTML = `
       <div class="card m-2 p-1">
          <img src="${producto.imagen}" alt="${producto.descripcion}" class=" mt-3  imagenProducto">
@@ -55,8 +63,11 @@ const mostrarProductos = () => {
              <button class="btn botonEstilizado" id="boton${producto.id}">Agregar al carrito <i class="fas fa-shopping-cart"></i></button>
          </div>
       </div>`
+
+    // A CONTINUACION, LE INDICO AL HTML DONDE SE VA A UBICAR ESE CONTENEDOR QUE CREE EN LA LINEA 52, QUE ES DENTRO DEL OTRO CONTENEDOR PRINCIPAL
     cajaProductos.appendChild(tarjeta);
 
+    // RELACIONO POR MEDIO DE UNA CONSTANTE LOCAL, EL BOTON DINAMICO PARA AGREGAR AL CARRITO CADA PRODUCTO
     const boton = document.querySelector(`#boton${producto.id}`);
     boton.addEventListener("click", () => {
       agregarAlCarrito(producto.id);
@@ -64,9 +75,12 @@ const mostrarProductos = () => {
   })
 }
 
+// INVOCO LA FUNCION PREVIA
 mostrarProductos()
 
+// AHORA CREO LA FUNCION PARA AGREGAR AL CARRITO LOS PRODUCTOS DESEADOS
 const agregarAlCarrito = (id) => {
+  // POR MEDIO DEL METODO FIND BUSCO SI EL PRODUCTO SELECCIONADO YA SE ENCUENTRA EN EL CARRITO O NO, PARA EVITAR LA DOBLE CARGA DEL MISMO, YA QUE EN CASO DE REPETIRSE, QUIERO QUE MUESTRE UN INCREMENTO EN SU CANTIDAD, PERO NO UNA DUPLICACION DE TARJETA. SI NO ESTA REPETIDO, ENTONCES SI LO AGREGO AL MISMO
   const productoEnCarrito = carrito.find(producto => producto.id === id);
   if (productoEnCarrito) {
     productoEnCarrito.cantidad++;
@@ -74,22 +88,29 @@ const agregarAlCarrito = (id) => {
     const producto = productos.find(producto => producto.id === id)
     carrito.push(producto);
   }
+
+  // GUARDO TODOS LOS ELEMENTOS DEL CARRITO EN EL LOCAL STORAGE
   localStorage.setItem("carrito", JSON.stringify(carrito));
   calcularElTotal();
 }
 
-
-
+// CREO EL EVENTO EN EL BOTON DE VER EL CARRITO, PARA QUE ESTE LO MUESTRE MEDIANTE LA SIGUIENTE FUNCION QUE VOY A CREAR
 mostrarCarrito.addEventListener("click", () => {
   mostrarElCarrito();
 })
 
+// CREO LA FUNCION QUE MUESTRA EL CARRITO
 const mostrarElCarrito = () => {
+
+  // ANTES QUE NADA, LE INSERTO UN STRING VACIO PARA QUE CADA VEZ QUE OPRIMA EL BOTON PARA VISUALIRAR EL CARRITO, NO SE MULTIPLIQUE LA VISUALIZACION DEL MISMO SIN SENTIDO
   cajaCarrito.innerHTML = "";
+
+  // ITERO SOBRE LOS PRODUCTOS, IGUAL QUE EN LA FUNCION ANTERIOR, LUEGO CREO UN ELEMENTO MEDIANTE EL DOM QUE CONTENGA CADA UNO DE LOS MISMOS A TRAVES DE BOOTSTRAP
   carrito.forEach(producto => {
     const tarjeta = document.createElement("div");
     tarjeta.classList.add("col-xl-3", "col-md-6", "col-xs-12");
 
+    // CREO UNA CONSTANTE PARA CALCULAR EL TOTAL DE CADA PRODUCTO SEGUN LA CANTIDAD ELEGIDA DEL MISMO
     const total = producto.precio * producto.cantidad;
 
     tarjeta.innerHTML = `
@@ -105,42 +126,59 @@ const mostrarElCarrito = () => {
       </div>
       `
 
+    // NUEVAMENTE LE INDICO AL CONTENEDOR DE CADA PRODCUTO DONDE DEBE UBICARSE EN EL HTML 
     cajaCarrito.appendChild(tarjeta);
 
+    // CREO UN BOTON PARA ELIMINAR LOS PRODUCTOS, INVOCANDO UNA FUNCION QUE CREARÉ A CONTINUACION
     const eliminar = document.querySelector(`#eliminar${producto.id}`);
     eliminar.addEventListener("click", () => {
       eliminarProducto(producto.id);
     })
   })
+
+  // INVOCO UNA FUNCION PARA CALCULAR EL TOTAL DEL CARRITO QUE CREARÉ MAS ADELANTE
   calcularElTotal();
 }
 
-const eliminarProducto = (id) => {
-  const productoEliminado = carrito.find(producto => producto.id === id);
-  let idProducto = carrito.indexOf(productoEliminado);
-  carrito.splice(idProducto, 1);
-  mostrarElCarrito();
-
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-}
-
-const vaciarElCarrito = document.querySelector("#vaciarElCarrito");
-vaciarElCarrito.addEventListener("click", () => {
-  vaciarCarritoCompleto();
-});
-
-
-const vaciarCarritoCompleto = () => {
-  carrito = [];
-  localStorage.clear();
-  mostrarElCarrito();
-}
-
-
+// FUNCION QUE CALCULA EL TOTAL DE CADA PRODUCTO TENIENDO EN CUENTA SU VALOR Y LA CANTIDAD DE CADA UNO POR MEDIO DE UN METODO FOR EACH PARA ITERAR EN ELLOS Y DECIRLE QUE EL TOTAL SERA EL RESULTADO DEL PRECIO DEL MISMO POR LA CANTIDAD ELEGIDA
 const calcularElTotal = () => {
   let totalCompra = 0;
   carrito.forEach(producto => {
     totalCompra += producto.precio * producto.cantidad;
   })
   montoTotalDelCarrito.innerHTML = `$${totalCompra}`
+}
+
+// FUNCION PARA ELIMINAR PRODUCTO QUE FUNCIONA CON EL ID DEL PRODUCTO COMO PARAMETRO
+const eliminarProducto = (id) => {
+  const productoEliminado = carrito.find(producto => producto.id === id);
+
+  // CREO UNA VARIABLE PARA IDENTIFICAR AL PRODUCTO QUE DESEO ELIMINAR POR MEDIO DE SU ID
+  let idProducto = carrito.indexOf(productoEliminado);
+
+  // ELIMINO EL PRODCUTO SELECCIONADO METODO SPLICE MEDIANTE
+  carrito.splice(idProducto, 1);
+
+  // INVOCO NUEVAMENTE LA FUNCION DE MOSTRAR EL CARRITO
+  mostrarElCarrito();
+
+  // UNA VEZ ELIMINADO EL O LOS PRODUCTOS DESEADOS, ACTUALIZO EL CARRITO EN EL LOCAL STORAGE
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+// CREO UNA CONSTANTE PARA VACIAR EL CARRITO COMPLETO A TRAVES DE UN BOTON QUE INVOCARA UNA FUNCION DE VACIADO
+const vaciarElCarrito = document.querySelector("#vaciarElCarrito");
+vaciarElCarrito.addEventListener("click", () => {
+  vaciarCarritoCompleto();
+});
+
+// LA FUNCION DE VACIADO DEL CARRITO SE EJECUTA A TRAVES DE LA MODIFICACION DEL CONTENIDO DEL ARRAY DEL CARRITO, INGRESANDOLO VACIO
+const vaciarCarritoCompleto = () => {
+  carrito = [];
+
+  // UTILIZO EL METODO CLEAR PARA LIMPIAR EL LOCAL STORAGE
+  localStorage.clear();
+
+  // NUEVAMENTE MUESTRO EL CARRITO CON LAS MODIFICACIONES
+  mostrarElCarrito();
 }
